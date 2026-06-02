7cd90a9f682ed7f93ae6ff5459f69ed4a32cb221
Kanta Oikawa <oikawa_kanta@cyberagent.co.jp>
2026-06-02 22:37:29 +0900

    フォルダ構成を整理

---
+++ terraform/google/common/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.34.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "7.34.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.google_cloud_project_id
+  region  = var.google_cloud_region
+}
+
+provider "google-beta" {
+  project = var.google_cloud_project_id
+  region  = var.google_cloud_region
+}
