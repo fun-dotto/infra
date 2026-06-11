f91a6d6f82e4bcf0f709e95587b0675f60808b33
Kanta Oikawa <iam@kantacky.com>
2026-06-12 00:05:49 +0900

    terraform/google/project でGCSバックエンドを追加

--- terraform/google/project/provider.tf
+++ terraform/google/project/provider.tf
@@ -1,1 +1,27 @@
-../common/provider.tf
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.34.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "7.34.0"
+    }
+  }
+
+  backend "gcs" {
+    bucket = "swift2023groupc-tfstate"
+    prefix = "project"
+  }
+}
+
+provider "google" {
+  project = var.google_cloud_project_id
+  region  = var.google_cloud_region
+}
+
+provider "google-beta" {
+  project = var.google_cloud_project_id
+  region  = var.google_cloud_region
+}
