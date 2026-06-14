e400610e61fc6aafa189ea26c542728c6a5b7627
Kanta Oikawa <oikawa_kanta@cyberagent.co.jp>
2026-06-14 12:27:07 +0900

    Secret Managerの権限を設定 (#7)

--- terraform/google/modules/secret-manager/app-dart-define-production.tf
+++ terraform/google/modules/secret-manager/app-dart-define-production.tf
@@ -5,3 +5,9 @@ resource "google_secret_manager_secret" "app_dart_define_production" {
     auto {}
   }
 }
+
+resource "google_secret_manager_secret_iam_member" "app_dart_define_production" {
+  secret_id = google_secret_manager_secret.app_dart_define_production.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = local.app_secret_accessor_member
+}
--- terraform/google/modules/secret-manager/app-env-fastlane-android.tf
+++ terraform/google/modules/secret-manager/app-env-fastlane-android.tf
@@ -5,3 +5,9 @@ resource "google_secret_manager_secret" "app_env_fastlane_android" {
     auto {}
   }
 }
+
+resource "google_secret_manager_secret_iam_member" "app_env_fastlane_android" {
+  secret_id = google_secret_manager_secret.app_env_fastlane_android.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = local.app_secret_accessor_member
+}
