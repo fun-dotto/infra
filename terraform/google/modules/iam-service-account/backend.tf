9062cb56017a0a31f31785c5d4969fb6154b408b
Kanta Oikawa <oikawa_kanta@cyberagent.co.jp>
2026-06-20 22:17:54 +0900

    移行・廃止予定のService Accountの定義を削除

--- terraform/google/modules/iam-service-account/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "swift2023groupc-tfstate"
-    prefix = "iam-service-account"
-  }
-}
--- terraform/google/modules/iam-service-account/fastlane-supply.tf
+++
@@ -1,5 +0,0 @@
-resource "google_service_account" "fastlane_supply" {
-  account_id   = "fastlane-supply"
-  description  = "Account used to give fastlane supply access to Google Play Console."
-  display_name = "fastlane-supply"
-}
