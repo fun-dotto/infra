f0aac0250a75aa7f48e64c35d3a96d0a523a9529
Hikaru Saito <135803012+hikaru-0602@users.noreply.github.com>
2026-07-15 11:44:40 +0900

    Update terraform/google/modules/secret-manager/gtfs_realtime_url_hakodate_bus.tf
    Co-authored-by: Kanta Oikawa <oikawa_kanta@cyberagent.co.jp>

--- terraform/google/modules/secret-manager/gtfs_realtime_url_hakodate_bus.tf
+++ terraform/google/modules/secret-manager/gtfs_realtime_url_hakodate_bus.tf
@@ -1,4 +1,4 @@
-resource "google_secret_manager_secret" "gtfs_realtime" {
+resource "google_secret_manager_secret" "gtfs_realtime_url_hakodate_bus" {
   secret_id = "gtfs_realtime_url_hakodate_bus"
 
   replication {
