5364da2d3fd48b67f2df59bdad503d5213f3008b
Hikaru Saito <135803012+hikaru-0602@users.noreply.github.com>
2026-07-15 11:44:46 +0900

    Update terraform/google/modules/secret-manager/gtfs_schedule_url_hakodate_bus.tf
    Co-authored-by: Kanta Oikawa <oikawa_kanta@cyberagent.co.jp>

--- terraform/google/modules/secret-manager/gtfs_schedule_url_hakodate_bus.tf
+++ terraform/google/modules/secret-manager/gtfs_schedule_url_hakodate_bus.tf
@@ -1,4 +1,4 @@
-resource "google_secret_manager_secret" "gtfs_schedule" {
+resource "google_secret_manager_secret" "gtfs_schedule_url_hakodate_bus" {
   secret_id = "gtfs_schedule_url_hakodate_bus"
 
   replication {
