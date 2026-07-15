f593c0969e35e639ce79bd293c72dec6a4294449
Hikaru Saito <135803012+hikaru-0602@users.noreply.github.com>
2026-07-15 10:23:45 +0900

    Update terraform/google/modules/secret-manager/gtfs-schedule.tf
    Co-authored-by: Kanta Oikawa <oikawa_kanta@cyberagent.co.jp>

--- terraform/google/modules/secret-manager/gtfs-schedule.tf
+++ terraform/google/modules/secret-manager/gtfs-schedule.tf
@@ -5,9 +5,3 @@ resource "google_secret_manager_secret" "gtfs_schedule" {
     auto {}
   }
 }
-
-resource "google_secret_manager_secret_iam_member" "gtfs_schedule" {
-  secret_id = google_secret_manager_secret.gtfs_schedule.secret_id
-  role      = "roles/secretmanager.secretAccessor"
-  member    = local.app_secret_accessor_member
-}
