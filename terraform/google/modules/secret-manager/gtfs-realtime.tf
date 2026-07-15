1cb646ae7288b019ef5c318f340ff04bccbc7ae1
Hikaru Saito <135803012+hikaru-0602@users.noreply.github.com>
2026-07-15 10:23:53 +0900

    Update terraform/google/modules/secret-manager/gtfs-realtime.tf
    Co-authored-by: Kanta Oikawa <oikawa_kanta@cyberagent.co.jp>

--- terraform/google/modules/secret-manager/gtfs-realtime.tf
+++ terraform/google/modules/secret-manager/gtfs-realtime.tf
@@ -5,9 +5,3 @@ resource "google_secret_manager_secret" "gtfs_realtime" {
     auto {}
   }
 }
-
-resource "google_secret_manager_secret_iam_member" "gtfs_realtime" {
-  secret_id = google_secret_manager_secret.gtfs_realtime.secret_id
-  role      = "roles/secretmanager.secretAccessor"
-  member    = local.app_secret_accessor_member
-}
